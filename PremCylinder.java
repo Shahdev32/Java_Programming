d35d4d127f8389563fc7e2db55641f5dea2a37a7
shahdev kumar <shahdevkumar.d@gmail.com>
2024-04-06 12:29:18 +0500

    premiter of polygon

---
+++ PremCylinder.java
@@ -0,0 +1,24 @@
+// Java program to find 
+// perimeter of cylinder 
+import java.io.*; 
+
+class PremCylinder { 
+
+	// Function to calculate perimeter 
+	static int perimeter(int diameter, int height) 
+	{ 
+		return 2 * (diameter + height); 
+	} 
+	
+	/* Driver program to test above function */
+	public static void main(String[] args) 
+	{ 
+		int diameter = 5; 
+		int height = 10; 
+		System.out.println("Perimeter = " + 
+						perimeter(diameter, height) 
+									+ " units\n"); 
+	} 
+} 
+
+
